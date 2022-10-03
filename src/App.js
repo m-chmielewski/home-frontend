709759dda9ebee80c6889de81f3ff1b1613d217e
Maciej <mitek.chmielewski@gmail.com>
2022-10-03 17:09:23 +0200

    mobile navbar addded

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./Shared.css";
+// import "./Shared.css";
 
 import Home from "./Pages/Home";
 
